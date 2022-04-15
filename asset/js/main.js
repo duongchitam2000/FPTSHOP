e5269ac78495edb90c2aa5bfcd31abbc1a8dae77
duongchitam2000 <tann12391@gmail.com>
2022-04-15 09:36:34 +0700

    update final

--- asset/js/main.js
+++ asset/js/main.js
@@ -16,3 +16,34 @@ closeMenu.onclick = function(){
     modalMenu.style = `display: none`;
     menuReponsive.style = `transform: translateX(-100%)`;
 }
+
+// product tab
+const tabsall = document.querySelectorAll('.product-list-tab-item')
+const tabsContentall = document.querySelectorAll('.product-cate-card')
+tabsall.forEach((tab, index) =>{
+	tab.addEventListener("click", () =>{
+		tabsContentall.forEach((tabContent)=>{
+			tabContent.classList.remove('active')
+		}) 
+		tabsall.forEach((tab)=>{
+			tab.classList.remove('active')	
+		}) 
+		console.log(index);
+
+		tabsall[index].classList.add('active')
+		tabsContentall[index].classList.add('active')	
+		
+	})
+})
+
+const tabsall_product = document.querySelectorAll('.form-check-option')
+// const tabsContentall = document.querySelectorAll('.product-cate-card')
+tabsall_product.forEach((tab, index) =>{
+	tab.addEventListener("click", () =>{
+		tabsall_product.forEach((tabContent)=>{
+			tabContent.classList.remove('active')
+		}) 
+		tabsall_product[index].classList.add('active')
+
+	})
+})
